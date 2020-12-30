0aa22419f093745e99d23ed0c986e0dd3f966118
haolaoshi <wantong-liu@163.com>
2020-12-29 23:20:54 -0500

    yestoday

--- c01/27-detab.c
+++ c01/27-detab.c
@@ -10,4 +10,33 @@
  * Assume a fixed set of tab stops,say every n columns.
  * Should be a variable or a symbolic parameter ? 
  */
+#include <stdio.h>
 
+#define TABSPACE    4
+
+int getaline();
+
+main()
+{
+    int c,len;
+    len = 0;
+    while((len = getaline()) > 0){
+       printf("."); 
+    }
+    if(len == -1) printf("Exit while");
+    else printf("Exception occurs.");
+}
+void printtab(){
+    int i = 0;
+    while(i++<TABSPACE) printf("_");
+}
+int getaline()
+{
+    int c;
+
+    while((c = getchar()) != EOF && c != '\n'){
+        if(c == '\t') printtab(); 
+        else printf("%c",c);
+    }
+    return c == EOF ? -1 : 1;
+}
