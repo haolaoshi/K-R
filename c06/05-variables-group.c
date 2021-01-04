1c8f9cd569c1c4595c2d99a94893bfaac4d02e9c
haolaoshi <liujiahao.oak@gmail.com>
2021-01-04 10:01:53 -0500

    group

--- c06/05-variables-group.c
+++ c06/05-variables-group.c
@@ -6,4 +6,87 @@
  * Make 6 a parameter that can be set from the command line.
  */
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
+
+#define MAXWORD 100 
+struct tnode{
+    char* word;
+    int   count;
+    tnode*  left;
+    tnode*  right;
+};
+
+struct tnode* root;
+struct tnode* addnode(char *, struct tnode *, int num);
+
+int identical = 6;
+
+ int main(int argc,char* argv[])
+ {
+    if(argc > 1)
+        identical = atoi(argv[1]);
+
+    if(identical <= 1)
+        return -1;
+
+    char* word;
+    while(getword(word,MAXWORD) != EOF){
+        if(isalpha(word[0])
+            addNode(word,root,identical);
+    }
+ }
+
+#define BUFFSIZE    1000
+char buff[BUFFSIZE];
+int bufferp = 0;
+
+int getch()
+{
+    return bufferp > 0 ? buff[--bufferp] : getchar();
+}
+
+void ungetch(char c)
+{
+    if(bufferp >= BUFFSIZE)
+        printf("too many characters!");
+    else
+        buff[bufferp++] = c;
+}
+
+char* getword(char* word,int max)
+{
+    char* w = word;
+    int n ;
+    int c ;
+    while(isblank(c = getch()));
+
+    if(c != EOF)
+        *w++ = c;
+
+    if(!isalpha(c)){
+        *w = '\0';
+        return c;
+    }
+
+    for(; max-->0;w++){
+        if(!isalnum(c =getch())){
+            ungetch(c);
+            break;
+        }
+
+    }
+    *w = '\0';
+    return w[0];
+}
+
+
+struct tnode* addnode(char *, struct tnode *, int num)
+{
+    
+
+
+}
