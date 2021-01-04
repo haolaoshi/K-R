447152ddb80ef7e6895577d3b969c8b27730b28a
haolaoshi <liujiahao.oak@gmail.com>
2021-01-04 00:45:56 -0500

    exers

---
+++ c06/06-cross-reference.c
@@ -0,0 +1,7 @@
+/*
+ * Exercise 6-3.
+ * Write a cross-referencer that prints a list of all words in a document,
+ * and,for each word, a list of the line numbers on which it occurs.
+ * Remove noise words like "thie " ,"and" ,and so on.
+ */
+
---
+++ c06/07-sorting-frequency.c
@@ -0,0 +1,6 @@
+/*
+ * Exercise 6-4/
+ * Write a program that prints the distinct words int its input sorted into decreasing order of requency of occurrence.
+ * Precede each word by its count.
+ */
+
