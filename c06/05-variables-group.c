5b2bd8e4e38af3c851a679ca1eafe1531d8b984b
haolaoshi <liujiahao.oak@gmail.com>
2021-01-05 01:34:14 -0500

    6.2

--- c06/05-variables-group.c
+++ c06/05-variables-group.c
@@ -16,57 +16,28 @@
 struct tnode{
     char* word;
     int   count;
-    tnode*  left;
-    tnode*  right;
+    struct tnode*  left;
+    struct tnode*  right;
 };
 
 struct tnode* root;
-struct tnode* addnode(char *, struct tnode *, int num);
+struct tnode* addNode(char *, struct tnode *, int num);
 
 int identical = 6;
 
- int main(int argc,char* argv[])
- {
-    if(argc > 1)
-        identical = atoi(argv[1]);
-
-    if(identical <= 1)
-        return -1;
-
-    char* word;
-    while(getword(word,MAXWORD) != EOF){
-        if(isalpha(word[0])
-            addNode(word,root,identical);
-    }
- }
-
-#define BUFFSIZE    1000
-char buff[BUFFSIZE];
-int bufferp = 0;
-
-int getch()
-{
-    return bufferp > 0 ? buff[--bufferp] : getchar();
-}
-
-void ungetch(char c)
-{
-    if(bufferp >= BUFFSIZE)
-        printf("too many characters!");
-    else
-        buff[bufferp++] = c;
-}
-
-char* getword(char* word,int max)
+char getword(char* word,int max)
 {
     char* w = word;
     int n ;
     int c ;
-    while(isblank(c = getch()));
 
-    if(c != EOF)
+    while(isspace(c = getch()))
+        ;
+
+    if(c != EOF){
         *w++ = c;
-
+    }
+    
     if(!isalpha(c)){
         *w = '\0';
         return c;
@@ -83,10 +54,79 @@ char* getword(char* word,int max)
     return w[0];
 }
 
-
-struct tnode* addnode(char *, struct tnode *, int num)
+void printnode(struct tnode *t)
 {
-    
+    if(t ! = NULL){
+        printnode(t->left);
+        printf("%s:%d\n",t->word,t->count);
+        printnode(t->right);
+    }
+}
 
+ int main(int argc,char* argv[])
+ {
+    if(argc > 1)
+        identical = atoi(argv[1]);
+    if(identical <= 1)
+        return -1;
+    char* word[MAXWORD];
+    while(getword(word,MAXWORD) != EOF){
+        if(isalpha(word[0]))
+            addNode(word,root,identical);
+    }
+    printnode(root);
+ }
+
+#define BUFFSIZE    1000
+char buff[BUFFSIZE];
+int bufferp = 0;
+
+int getch()
+{
+    return  bufferp > 0 ? buff[--bufferp] : getchar();
+}
+
+void ungetch(char c)
+{
+    if(bufferp >= BUFFSIZE)
+        printf("too many characters!");
+    else
+        buff[bufferp++] = c;
+}
+
+struct tnode* talloc()
+{
+    return (struct tnode*) malloc(sizeof(struct tnode));
 
 }
+
+struct tnode* addNode(char *word, struct tnode *t, int num)
+{
+    char *strdup(char *);
+
+printf("\twill add node %s\n",word);
+   if(t == NULL){
+        t= talloc();
+        t->word = strdup(word);
+        t->count = 1;
+        t->left = NULL;
+        t->right = NULL;
+   }else if(strcmp(t->word,word)> 0){
+        addNode(word,t->right,num);
+   }else if(strcmp(t->word,word)<0){
+        addNode(word,t->left,num);
+   }else{
+        t->count++;
+   }
+}
+
+char *strdup(char *s)
+{
+    char *p;
+    p = (char *)malloc(strlen(s) + 1);
+    if( p != NULL)
+        strcpy(p,s);
+    return p;
+}
+
+
