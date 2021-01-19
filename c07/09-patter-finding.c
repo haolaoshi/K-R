f93e45cfeeb4c68f343d44e554b47b336100c14f
haolaoshi <liujiahao.oak@gmail.com>
2021-01-19 00:07:11 -0500

    chapter7

---
+++ c07/09-patter-finding.c
@@ -0,0 +1,9 @@
+/*
+ * Exercise 7-7.
+ * Modify the pattern finding program of Chatper 5 to take its input from a set of named files or ,
+ * if no files are named as arguments,
+ * from the standard input ,
+ * Should the file name be printed when a matching line is found ?
+ */
+
+
