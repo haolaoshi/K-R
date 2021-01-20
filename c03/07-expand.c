8bc5e94bf841ab64386e0d6cf070ffad08d9e72f
haolaoshi <wantong-liu@163.com>
2021-01-20 09:04:29 -0500

    expand

--- c03/07-expand.c
+++ c03/07-expand.c
@@ -12,23 +12,42 @@
 //assuming s2 length is enough.
 void expand(char s1[],char s2[])
 {
+   printf("%s\n",s1);
    int i = 0 ;
    int j = 0;
     char c = 0;
-    int head = 0;
     int len = strlen(s1) - 1;
     for(i=0,j=0; s1[i]; i++,j++){
-        if(!head && (s1[i] != '-')) s2[j] = s1[i];
-        else if(i>0 && (s1[i] = '-')&& i < len){
-           if(s1[i+1]> 'z' || s1[i+1] < 'a') s2[j] = s1[i];
-           else{
-              for(c = s1[i-1]; s1[i+1] < 'z' && s1[i+1] > 'a'; c++){
-
-                 s[j++] = c;
+        if((s1[i] != '-')) {
+            s2[j] = s1[i];
+   //         printf("(%d=%i,%c)",j,i,s2[j]); 
+        }else if(i>0 && (s1[i] = '-')&& i < len){
+           if(s1[i+1]> 'z' || s1[i+1] < 'a') {
+               s2[j] = s1[i];
+    ///           printf("[%d=%d],%c",j,i,s2[j]);
+           } else{
+      //          printf("\n\t");
+              for(c = s1[i-1]+ 1 ; c < s1[i+1];j++, c++){
+                 s2[j] = c;
+       //          printf("{%d,%d},%c ",j,i,s2[j]);
               }
+              printf("\n");
+              j--;
            }
         }
 
     }
+   s2[j] = '\0'; 
+  //printf("\n i = %d ,len = %d, j = %d",i,len,j);
+}
+
+main()
+{
+    char s1[] = "I Can Say a-z songs.";
+
+    char s2[1000];
+
+    expand(s1,s2);
+    printf("%s\n",s2);
 
 }
