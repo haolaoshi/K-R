6bd380ae6c80c22410ac4bd609e866a667c65923
haolaoshi <liujiahao.oak@gmail.com>
2021-01-30 00:01:37 -0500

    backpag

---
+++ c05/14-etab.c
@@ -0,0 +1,10 @@
+/*
+ *
+ * Exercise 5-12.
+ *      Extend entab and detab to accept the shorthand 
+ *              entab -m +n
+ *      to mean tab stops every n columns,starting at column m .
+ *      Choose convenient for the user default behavior
+ */
+
+
