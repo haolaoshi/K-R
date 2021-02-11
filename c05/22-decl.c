b7eaf358f55946218d4c62f40af0caef39abc04a
haolaoshi <liujiahao.oak@gmail.com>
2021-02-11 05:16:24 -0500

    decl

--- c05/22-decl.c
+++ c05/22-decl.c
@@ -5,7 +5,24 @@
  *
  *
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+#define MAXTOKEN    100
+enum {NAME,PARENS,BRACKETS};
+
+void dcl(void);
+void dirdcl(void);
+int gettoken(void);
+int tokentype;
+char token[MAXTOKEN];
+char name[MAXTOKEN];
+char datatype[MAXTOKEN];
+char out[100];
+
+/*parse a direct declarator */
 void dirdcl(void)
 {
     int type;
@@ -13,7 +30,7 @@ void dirdcl(void)
         dcl();
         if(tokentype != ')')
             printf("error, missing ) \n");
-    else if(tokentype == NAME)
+    }else if(tokentype == NAME)
         strcpy(name,token);
     else
         printf("Error: expected name or (dcl) \n");
@@ -30,17 +47,17 @@ void dirdcl(void)
 
 }
 
+/*parse a declarator */
 void dcl(void)
 {
 
     int ns;
-    for(ns =0; gottoken() == '*'; )
+    for(ns =0; gettoken() == '*'; ) // count star 
         ns++;
     dirdcl();
 
     while(ns-- > 0)
         strcat(out,"  pointer to");
-
 
 }
 
@@ -53,10 +70,53 @@ main()
         if(tokentype != '\n')
             printf("syntax error\n");
         printf("%s : %s %s \n",name,out,datatype);
-
-
     }
-
     return 0;
 }
+
+int gettoken(void)
+{
+    int c,getch(void);
+    void ungetch(int);
+    char *p = token;
+
+    while((c = getch()) == ' ' || c == '\t')
+        ;
+    if(c == '('){
+        if((c = getch()) == ')'){
+            strcpy(token,"()");
+            return tokentype = PARENS;
+        }else{
+            ungetch(c);
+            return tokentype = '(';
+        }
+    }else if(c == '['){
+        for(*p++ = c;(*p++ = getch()) != ']';)
+            ;
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    }else
+        return tokentype = c;
+
+}
+#define BUFFSIZE	100
+
+char buf[BUFFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if(bufp >= BUFFSIZE)
+		printf("ungetch : too many characters.\n");
+	else
+		buf[bufp++] = c;
+}
+
 
