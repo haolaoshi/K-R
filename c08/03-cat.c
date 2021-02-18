8ede0a73207d6b354cc2426777d64b3e4ebd6766
haolaoshi <liujiahao.oak@gmail.com>
2021-02-18 00:00:13 -0500

    unix

---
+++ c08/03-cat.c
@@ -0,0 +1,10 @@
+/*
+ *
+ * Exercise 8-1:
+ * Rewrite the program cat from chapter 7 using read , write , open and close instead of their standard library equivalents.
+ * Perform experiments to detyermin the relativer speeds of the rwo version.
+ *
+ */
+
+
+
---
+++ c08/04-lseek.c
@@ -0,0 +1,9 @@
+
+
+int get(int fd,long pos,char *buf,int n)
+{
+    if(lseek(fd,pos,0) >= 0)
+        return read(fd,buf,n);
+    else
+        return -1;
+}
