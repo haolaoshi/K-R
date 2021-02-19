6b360401d5bb360920d42a264cfd923c943ef1ef
haolaoshi <liujiahao.oak@gmail.com>
2021-02-19 00:16:18 -0500

    func

---
+++ c08/06-flushbuf.c
@@ -0,0 +1,5 @@
+/*
+ *
+ * Exercise 8-3:
+ * Design and write _flushbuf,fflush,and fclose
+ */
---
+++ c08/08-fillbuf.c
@@ -0,0 +1,6 @@
+/*
+ *
+ * Exercise 8-2
+ * Rewrite fopen and _fillbuf with fields instead of explicit bit operatoion.
+ */
+
