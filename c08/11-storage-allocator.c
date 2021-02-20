c860a52c9fff2367e081ca6a904f6ac010abf014
haolaoshi <liujiahao.oak@gmail.com>
2021-02-20 00:14:30 -0500

    storage

--- c08/11-storage-allocator.c
+++ c08/11-storage-allocator.c
@@ -3,3 +3,17 @@
  *
  *
  */
+typedef long Align;
+
+union header{
+    struct {
+        union header *ptr;
+        unsigned size;
+
+    }s;
+    Align x;
+};
+typedef union header Header;
+
+static Header base;
+static Header *freep = NULL;
